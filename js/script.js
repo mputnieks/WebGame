8534a99d0c3683c090d65a9b6376aa960b0c7275
mputnieks <mputnieks@DX02043382.ri.riga.lv>
2020-02-19 09:23:54 +0200

    remote push

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 function makeTiles(){
-    let i = document.getElementByClassName("tile").length;
     let divs = document.createElement("DIV"); 
     divs.setAttribute("class","tile");
-    divs.innerHTML = (i+1)+")";
     let tile = document.createElement("DIV");
     tile.style = "text-align: center";
     tile.value = "tile";
