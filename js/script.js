757287dd7aaa2f9b0123aca310e4a91941d878fe
mputnieks <mputnieks@DX02043382.ri.riga.lv>
2020-02-26 09:29:42 +0200

    remote push

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
-let divs = document.createElement("DIV");
+let divs;
 let l = 3;                                  //l ir speles laukuma malas garums (blokos)
 let row;
 
 function startGame(){                       // izsauc šo, kad uzspiež pogu start
-    divs.setAttribute("class","tile");
-    for (let i = 0; i < l; i++) {
-        makeRow();
-    }
-    
-//    while (document.getElementById("forma").hasChildNodes()) {
-//        console.log("iet");
-//        document.getElementById("forma").removeChild(document.getElementById("forma").firstChild);
-//    }
-    
-    document.getElementById("forma").appendChild(divs);
+    l = document.getElementById("quantity").value;
+    divs = document.createElement("DIV");
+        while (document.getElementById("forma").hasChildNodes()) {
+            console.log("iet");
+            document.getElementById("forma").removeChild(document.getElementById("forma").firstChild);
+        }
+        divs.setAttribute("class","tile");
+        for (let i = 0; i < l; i++) {
+            makeRow();
+        }   
+
+        document.getElementById("forma").appendChild(divs);
 }
 
 function makeRow(){                         // izveido jaunu rindu
@@ -26,10 +27,11 @@ function makeRow(){                         // izveido jaunu rindu
 
 function makeTile(){                        // izveido jaunu bloku (tile)
     let tile = document.createElement("DIV");
-    tile.style = "text-align: center; display: inline-block; padding: 10px; background-color: #f4511e; border-style: solid; border-color: black; border-width: 2px;";
-    tile.innerHTML = "tile";
+    tile.style = "text-align: center; display: inline-block; padding: 50px;  border-style: solid; border-color: black; border-width: 2px;";
+    //tile.innerHTML = "tile";
     tile.onclick=()=>{
-        tile.innerHTML = " ";
+        //tile.innerHTML = " ";
+        tile.src = "images/f1.jpg";
     };
     row.appendChild(tile);
 }
