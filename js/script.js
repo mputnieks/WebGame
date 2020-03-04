eaaa2f0b04bf1e6d0ab91e6b232775cbe2f5c271
mputnieks <mputnieks@DX02043382.ri.riga.lv>
2020-03-04 09:04:58 +0200

    Merge origin/master
    Conflicts:
    	js/script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let divs;
 let l = 3;                                  //l ir speles laukuma malas garums (blokos)
 let row;
+let m1 = [[]];                                     //Masivs 1 (Galvenais)
+let m2 = [];                                     //Masivs 2
+let m3 = [];                                     //Masivs 3
 
 function startGame(){                       // izsauc šo, kad uzspiež pogu start
     l = document.getElementById("quantity").value;
@@ -10,12 +13,16 @@ function startGame(){                       // izsauc šo, kad uzspiež pogu sta
             console.log("iet");
             document.getElementById("forma").removeChild(document.getElementById("forma").firstChild);
         }
+        while (m1.length > 0){
+            m1.pop();
+        }
         divs.setAttribute("class","tile");
         for (let i = 0; i < l; i++) {
-            makeRow();
-        }   
+            makeRow(i);
+        } 
 
         document.getElementById("forma").appendChild(divs);
+        //te saliek tile vertibas...
     }
     else{
         while (document.getElementById("forma").hasChildNodes()) {
@@ -28,21 +35,21 @@ function startGame(){                       // izsauc šo, kad uzspiež pogu sta
     }
 }
 
-function makeRow(){                         // izveido jaunu rindu
+function makeRow(e){                         // izveido jaunu rindu
     row = document.createElement("div");
     for(let i = 0; i<l ;i++){
-        makeTile();
+        let tile = document.createElement("DIV");
+        tile.style = "text-align: center; display: inline-block; padding: 50px;  border-style: solid; border-color: black; border-width: 2px;";
+        tile.onclick=()=>{
+            onClick(tile);
+        };
+        m1[e][i] = tile;
+        row.appendChild(tile);
     }
     divs.appendChild(row);
 }
 
-function makeTile(){                        // izveido jaunu bloku (tile)
-    let tile = document.createElement("DIV");
-    tile.style = "text-align: center; display: inline-block; padding: 50px;  border-style: solid; border-color: black; border-width: 2px;";
-    //tile.innerHTML = "tile";
-    tile.onclick=()=>{
-        //tile.innerHTML = " ";
-        tile.src = "images/f1.jpg";
-    };
-    row.appendChild(tile);
+function onClick (tile){
+    //tile.innerHTML = " ";
+    tile.src = "images/f1.jpg";
 }
